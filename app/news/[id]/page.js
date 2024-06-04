04a015ba9fcc08e3aaa3adaab8fe1995cf5f2777
vjiji <restmi0711@gmail.com>
2024-06-04 23:23:28 +0900

    뉴스페이지 라우팅 및 레이아웃 설정

--- app/news/[id]/page.js
+++
@@ -1,9 +0,0 @@
-export default function NewsDetailPage({ params }) {
-  const newsId = params.id;
-  return (
-    <>
-      <h1>News Page</h1>
-      <p>{newsId}</p>
-    </>
-  );
-}
---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <div id="error">
+      <h1>Not Found!</h1>
+      <p>해당 기사를 찾을 수 없습니다!</p>
+    </div>
+  );
+}
